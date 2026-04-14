c7694377a1cc95b49808dee56aac343cd8ef6ec8
Logan <logansmithp23@gmail.com>
2026-04-14 00:11:22 -0700

    Added average readings

--- goals2/LineSensor.py
+++ goals2/LineSensor.py
@@ -8,10 +8,21 @@ class IR:
     
 
 class LineSensor:
-    def __init__(self, io, pin1, pin2, pin3):
+    def __init__(self, io, pin1, pin2, pin3, BUFFER_LENGTH = 10):
         self.IR_1 = IR(io, pin1)
         self.IR_2 = IR(io, pin2)
         self.IR_3 = IR(io, pin3)
+        self.buffer = [] 
+        self.BUFFER_LENGTH = BUFFER_LENGTH
 
     def read(self):
-        return (self.IR_1.read(), self.IR_2.read(), self.IR_3.read())
+        reading = (self.IR_1.read(), self.IR_2.read(), self.IR_3.read())
+
+        self.buffer.append(reading)
+        if len(self.buffer) > self.BUFFER_LENGTH:
+            self.buffer.pop(0)
+
+        return reading 
+
+    def get_average(self):
+        return [round(sum([self.buffer[i][j] for i in range(len(self.buffer))])/len(self.buffer)) for j in range(3)]
